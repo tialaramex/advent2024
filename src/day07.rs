9dae61ca319d429d47de78a7254a94f751fc5667
Nick Lamb <git@tree.tlrmx.org>
2024-12-07 12:26:07 +0000

    Tidier and slightly more efficient day 7

--- src/day07.rs
+++ src/day07.rs
@@ -2,7 +2,7 @@ use history::readfile;
 
 type Num = u64;
 
-fn check(line: &str) -> Num {
+fn handle(line: &str) -> (Num, Num, Vec<Num>) {
     let (goal, rest) = line
         .split_once(": ")
         .expect("lines should have : separator");
@@ -11,14 +11,23 @@ fn check(line: &str) -> Num {
         .split_once(' ')
         .expect("lines should then have at least two numbers");
     let first: Num = first.parse().expect("should all be numbers");
-    let mut nums: Vec<Num> = rest.split(' ').map(|s| s.parse().unwrap()).collect();
+    let nums: Vec<Num> = rest.split(' ').map(|s| s.parse().unwrap()).collect();
+    (goal, first, nums)
+}
+
+fn check(line: &str) -> Num {
+    let (goal, first, mut nums) = handle(line);
     // Use nums as a stack
     nums.reverse();
     let mut sums: Vec<Num> = Vec::new();
     sums.push(first);
     while let Some(b) = nums.pop() {
-        let mut next: Vec<Num> = Vec::new();
+        let mut next: Vec<Num> = Vec::with_capacity(sums.len() * 3);
         while let Some(a) = sums.pop() {
+            // We only grow, so if we're too big we're done
+            if a > goal {
+                continue;
+            }
             next.push(a + b);
             next.push(a * b);
         }
@@ -41,22 +50,18 @@ pub fn a(filename: &str) {
 }
 
 fn three(line: &str) -> Num {
-    let (goal, rest) = line
-        .split_once(": ")
-        .expect("lines should have : separator");
-    let goal: Num = goal.parse().expect("goal should be a number");
-    let (first, rest) = rest
-        .split_once(' ')
-        .expect("lines should then have at least two numbers");
-    let first: Num = first.parse().expect("should all be numbers");
-    let mut nums: Vec<Num> = rest.split(' ').map(|s| s.parse().unwrap()).collect();
+    let (goal, first, mut nums) = handle(line);
     // Use nums as a stack
     nums.reverse();
     let mut sums: Vec<Num> = Vec::new();
     sums.push(first);
     while let Some(b) = nums.pop() {
-        let mut next: Vec<Num> = Vec::new();
+        let mut next: Vec<Num> = Vec::with_capacity(sums.len() * 3);
         while let Some(a) = sums.pop() {
+            // We only grow, so if we're too big we're done
+            if a > goal {
+                continue;
+            }
             next.push(a + b);
             next.push(a * b);
             let s = format!("{a}{b}");
